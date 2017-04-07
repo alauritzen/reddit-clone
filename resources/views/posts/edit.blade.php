f3c00c612a5878f9fa0b3fc898f1635d24f2d62a
Alan <alaurit@gmail.com>
2017-04-06 19:40:07 -0500

    added buttons to edit page

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -6,3 +6,6 @@
 <p>Title: <input name = "title" type = "text" value = "{{ old('title') }}"></p>
 <p>Post: <input name = "content" type = "textarea" value = "{{ old('content') }}"></p>
 <p>URL: <input name = "url" type = "url" value = "{{ old('url') }}"></p>
+
+<input type = "submit">
+<input type = "button" action = "{{action('PostsController@index')}}" value = "Cancel">
