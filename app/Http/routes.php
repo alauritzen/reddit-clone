2860c4e3c2a13e0f9fae9fba4d62c8356ad09864
Alan <alaurit@gmail.com>
2017-04-11 08:14:41 -0500

    Created Post model and hardcoded a orm-test in Routes file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,4 +29,20 @@ Route::get('/increment/{number}', function($number) {
 
 Route::resource('posts', 'PostsController');
 
+Route::get('orm-test', function() {
+    $post1 = new \App\Models\Post();
+    $post1 -> title = 'Eloquent is awesome!';
+    $post -> url = 'https://laravel.com/docs/5.1/eloquent';
+    $post -> content = 'It is super easy to create a new post.';
+    $post1 -> created_by = '1';
+    $post -> save();
+
+    $post2 = new \App\Models\Post();
+    $post2 -> title = 'Eloquent is really easy!';
+    $post2 -> url = 'https://laravel.com/docs/5.1/eloquent';
+    $post2 -> content = 'It is super easy to create a new post';
+    $post2 -> created_by = '2';
+    $post2 -> save();
+});
+
 
